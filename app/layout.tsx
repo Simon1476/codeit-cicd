49b1613c88bb24b18417ba639c6627ecc465ae20
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-09-02 16:25:13 +0900

    second

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {/* <Navbar /> */}
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
